ab85d0a486661f31678beb9e630292a50c85b52e
AJ Ruf <z0isch@gmail.com>
2026-07-01 16:00:58 +0200

    Intro

--- scenes/intro.lua
+++ scenes/intro.lua
@@ -15,12 +15,24 @@ end
 
 function M.draw()
   local title = "Ghost Loop"
-  local scale = 5
-  local tw    = usagi.measure_text(title) * scale
-  local _, th = usagi.measure_text(title)
-  local tx    = math.floor((usagi.GAME_W - tw) / 2)
-  local ty    = math.floor(usagi.GAME_H / 3 - th * scale / 2)
-  gfx.text_ex(title, tx, ty, scale, 0, gfx.COLOR_WHITE, 1)
+  gfx.rect_fill(0, 0, usagi.GAME_W, usagi.GAME_H, gfx.COLOR_BLACK)
+  local scale      = 5
+  local tw         = usagi.measure_text(title) * scale
+  local _, th      = usagi.measure_text(title)
+  local tx         = math.floor((usagi.GAME_W - tw) / 2)
+  local ty         = math.floor(usagi.GAME_H / 3 - th * scale / 2)
+
+  local wave_amp   = 3
+  local wave_speed = 3
+  local wave_gap   = 0.5
+  local cx         = tx
+  for i = 1, #title do
+    local ch = title:sub(i, i)
+    local cw = usagi.measure_text(ch) * scale
+    local cy = ty + math.floor(wave_amp * math.sin(usagi.elapsed * wave_speed + i * wave_gap))
+    gfx.text_ex(ch, cx, cy, scale, 0, gfx.COLOR_INDIGO, 1)
+    cx = cx + cw
+  end
 
   local w      = 200
   local race_x = math.floor((usagi.GAME_W - w) / 2)
