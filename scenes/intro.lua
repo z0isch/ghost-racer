6c71494b39d1ff437ae9fb7f2de5345d0c81b147
AJ Ruf <z0isch@gmail.com>
2026-07-01 15:42:41 +0200

    Format

--- scenes/intro.lua
+++ scenes/intro.lua
@@ -14,20 +14,17 @@ function M.update(dt)
 end
 
 function M.draw()
-  local tdata = track_data.TRACKS[State.active_track]
-  road.draw_track(tdata.map)
-
-  local title  = "Ghost Lap"
-  local scale  = 5
-  local tw     = usagi.measure_text(title) * scale
-  local _, th  = usagi.measure_text(title)
-  local tx     = math.floor((usagi.GAME_W - tw) / 2)
-  local ty     = math.floor(usagi.GAME_H / 3 - th * scale / 2)
+  local title = "Ghost Loop"
+  local scale = 5
+  local tw    = usagi.measure_text(title) * scale
+  local _, th = usagi.measure_text(title)
+  local tx    = math.floor((usagi.GAME_W - tw) / 2)
+  local ty    = math.floor(usagi.GAME_H / 3 - th * scale / 2)
   gfx.text_ex(title, tx, ty, scale, 0, gfx.COLOR_WHITE, 1)
 
   local w      = 200
   local race_x = math.floor((usagi.GAME_W - w) / 2)
-  if ui.button("RACE", race_x, usagi.GAME_H - 80, { w = w, scale = 3 }) then
+  if ui.button("RACE", race_x, usagi.GAME_H - 120, { w = w, scale = 3 }) then
     SceneGoto("race")
   end
 end
