e0c06cf0cde24084ea4428e0cd56a795c2b51c15
AJ Ruf <z0isch@gmail.com>
2026-07-02 13:46:59 +0200

    bigger

--- hud.lua
+++ hud.lua
@@ -3,7 +3,7 @@ local economy = require "economy"
 local M = {}
 
 function M.draw()
-  local scale          = 2
+  local scale          = 3
   local _, th          = usagi.measure_text("0")
   local bal_y          = 6
   local rate_y         = bal_y + th * scale + 3
@@ -13,9 +13,9 @@ function M.draw()
   local cash_w         = math.max(usagi.measure_text(money_text) * scale,
     usagi.measure_text(cash_rate_text))
 
-  local cash_x = (usagi.GAME_W - cash_w) / 2
+  local cash_x         = (usagi.GAME_W - cash_w) / 2
 
-  local show_rates = economy.owns_any_ghost()
+  local show_rates     = economy.owns_any_ghost()
 
   gfx.text_ex(money_text, cash_x, bal_y, scale, 0, gfx.COLOR_GREEN, 1)
   if show_rates then
