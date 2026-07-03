054a43dd649c02c0046c263b4f027c373509b1e4
AJ Ruf <z0isch@gmail.com>
2026-07-03 09:55:55 +0200

    You earn label

--- scenes/buy.lua
+++ scenes/buy.lua
@@ -109,8 +109,13 @@ function M.draw_shop()
     ui.rank_text(rank, rank, x + math.floor((w - usagi.measure_text(rank)) / 2), info_y, 1)
   end
 
+  info_y = info_y + 13
+  local you_earn_label = string.format("You earn $%d per Checkpoint and ©", economy.PAY)
+  ui.coin_text(you_earn_label, x, info_y, 1, gfx.COLOR_LIGHT_GRAY)
+  info_y = info_y + 13
+
   if State.tracks[State.active_track].ghosts > 0 then
-    info_y                 = info_y + 13
+    info_y                 = info_y + 3
     local ghost_earn_label = "Ghosts earn" .. string.format(" $%d per Checkpoint and ©", economy.PAY * rank_mult)
     ui.coin_text(ghost_earn_label, x, info_y, 1, gfx.COLOR_LIGHT_GRAY)
   end
